8fd201ba65776378fa3c040045079f5447cd2108
Michael Zijlstra <mzijlstra@mum.edu>
2015-04-28 14:58:57 -0500

    now parses a classlist directly copy pasted from infosys (and correctly finds 6 digit studentID)

--- csv2acts.php
+++ csv2acts.php
@@ -10,28 +10,29 @@ if (count ($argv) !== 2 || !preg_match("/.*\.csv/i" ,$argv[1])) {
 }
 
 
+# we expect the CSV file to be formatted like a copy pasted infosys classlist
 $lines = file($argv[1]);
 foreach($lines as $line) {
-	list($sid, $first, $last, $email) = str_getcsv($line);
 
-	# lines that do not start with a studentId are ignored
-	if (preg_match("/^(98|10)\d{4}$/", $sid)) {
+	# lines that do not start with an index and then a studentId are ignored
+    if (preg_match("/^\d+\s*,\s*0{3}-[19]\d-\d{4}/", $line)) {
+    	list($idx, $sid, $first, $middle, $last, $email) = str_getcsv($line);
 
 		# in /etc/passwd set the comment field to:
 		# full name,studentId,,,email
-		$comment = "$first $last,$sid,,,$email";
-		# cleanup lastname
-		$last = preg_replace("/[ ']/", "", $last);
-		#$username = strtolower($first[0] . $last);
-        $username = $sid;
+		$comment = "$first $middle $last,$sid,,,$email";
+
+        # transform social security formatted student ID into 6 digit 
+        $matches = array();
+        preg_match("/0{3}-([19]\d)-(\d{4})/", $sid, $matches);
+        $username = $matches[1] . $matches[2];
 
 		# students don't get their own group, but are in users and jail
 		exec("useradd -m -c '$comment' -N -g users -G jail $username");
 
 		# set passphrase
 		$pass = passwd();
-        $result = 0;
-		exec("echo '$pass\n$pass\n' | passwd $username > /dev/null 2>&1", $result);
+		exec("echo '$pass\n$pass\n' | passwd $username > /dev/null 2>&1");
 
 		# set home dir permissions so that jail works & logs are not endangered
 		exec("chown root:root /home/$username /home/$username/log");
@@ -43,7 +44,7 @@ foreach($lines as $line) {
 		
 		# create custom welcome message
 		$message = 
-"Dear $first $last,
+"Dear $first $middle $last,
 
 This is an automated email to notify you that the following account has been created for you on sftp://mumstudents.org
 
@@ -60,7 +61,7 @@ Best of luck,
 
 Mumstudents.org Automated Account Creator
 
-P.S. Please let professor Michael Zijlstra mzijlstra@mum.edu know if you want to try and break the security on mumstudents.org. I'm always happy to work with students to improve security. I'm not so happy to find out after the fact if a machine has been compromised (cracking is a crime, and there are consequences).
+P.S. Please let Professor Michael Zijlstra mzijlstra@mum.edu know if you want to try and break the security on mumstudents.org. I'm always happy to work with students to improve security. Just be sure to let me know _before_ you start.
 ";
 
 		#email the user about his newly created account
